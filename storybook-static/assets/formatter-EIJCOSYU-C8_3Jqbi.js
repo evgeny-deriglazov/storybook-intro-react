01e24d6cdda81ae343993def0cdbef8d6a6ec068
Evgeny <evgenyderiglazov@yandex.ru>
2026-06-12 08:35:51 +0300

    Deploy: build storybook

---
+++ storybook-static/assets/formatter-EIJCOSYU-C8_3Jqbi.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-CVhquEBH.js";import{b as t}from"./iframe-D_esHlWu.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-B72qdwk0.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
